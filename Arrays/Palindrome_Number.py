afa73fee2390e2d56666f1fd7a428343db5533c2
David Plascencia <dplascenciap@gmail.com>
2024-10-01 17:49:39 -0400

    Solved problem 003

---
+++ Arrays/Palindrome_Number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, x):
+        x_string    = str(x)
+        lex_x_sting = len(x_string)
+        if lex_x_sting == 1:
+            return True
+        left_index  = 0
+        right_index = lex_x_sting - 1
+        while left_index > right_index:
+            if x_string[left_index] != x_string[right_index]:
+                return False
+            left_index  += 1
+            right_index -= 1
+        return True
+
+solution = Solution()
+x = 121
+print (solution.isPalindrome(x))
